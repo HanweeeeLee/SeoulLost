3e92794efe18d24a44e496e816168a8748f4314b
Hanwe Lee <mobiledev@kakao.com>
2020-06-07 15:28:42 +0900

    수정사항  - pod 추가

---
+++ SeoulLost/Pods/Target Support Files/Pods-SeoulLost-SeoulLostUITests/Pods-SeoulLost-SeoulLostUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeoulLost_SeoulLostUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SeoulLost_SeoulLostUITests
+@end
---
+++ SeoulLost/Pods/Target Support Files/Pods-SeoulLost/Pods-SeoulLost-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeoulLost : NSObject
+@end
+@implementation PodsDummy_Pods_SeoulLost
+@end
