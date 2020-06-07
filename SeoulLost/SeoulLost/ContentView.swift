adea0b63446c7c2d0b7d5ae2844d6400a426ff37
Hanwe Lee <mobiledev@kakao.com>
2020-06-07 15:09:07 +0900

    수정사항  - 프로젝트추가

---
+++ SeoulLost/SeoulLost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeoulLost
+//
+//  Created by hanwe on 2020/06/07.
+//  Copyright © 2020 hanwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
