1108d2e13646d162ad2fd2abf50a428a990c1ac0
Hanwe Lee <mobiledev@kakao.com>
2020-06-21 15:01:52 +0900

    피커뷰 done 작업중

--- SeoulLost/SeoulLost/Tabs/LostItemView/LostItemTabView.swift
+++ SeoulLost/SeoulLost/Tabs/LostItemView/LostItemTabView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct LostItemTabView: View {
     @ObservedObject var viewModel: LostItemTabViewModel
     
+    @State var gender : String? = nil
+    @State var arrGenders = ["Male","Female","Unknown"]
+    @State var selectionIndex = 0
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -27,7 +31,13 @@ struct LostItemTabView: View {
     
     private var content: some View {
         return LoadingView(isShowing: self.$viewModel.isShowLoading) {
-            Text("test")
+            VStack {
+//                Text("\(self.viewModel.placeTxt)")
+//                Text("\(self.viewModel.lostArticleTypeTxt)")
+//                TextField("잃어버린 장소", text: self.$viewModel.placeTxt)
+                TextFieldWithPickerAsInputView(data: self.arrGenders, placeholder: "잃어버린 장소", selector: #selector(self.doneTapped), selectionIndex: self.$selectionIndex, text: self.$gender)
+                
+            }
         }
     }
     
@@ -35,4 +45,84 @@ struct LostItemTabView: View {
         return self
     }
     
+    @objc func doneTapped() {
+//        self.toolbarDelegate?.didTapDone()
+        print("test")
+    }
+}
+
+struct TextFieldWithPickerAsInputView : UIViewRepresentable {
+    
+    var data : [String]
+    var placeholder : String
+    var selector:Selector
+    
+    @Binding var selectionIndex : Int
+    @Binding var text : String?
+    
+    private let textField = UITextField()
+    private let picker = UIPickerView()
+    
+    func makeCoordinator() -> TextFieldWithPickerAsInputView.Coordinator {
+        Coordinator(textfield: self)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<TextFieldWithPickerAsInputView>) -> UITextField {
+        picker.delegate = context.coordinator
+        picker.dataSource = context.coordinator
+        picker.backgroundColor = .white
+        picker.tintColor = .black
+        textField.placeholder = placeholder
+        textField.inputView = picker
+        textField.delegate = context.coordinator
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: selector)
+//        let doneButton = UIBarButtonItem(title: <#T##String?#>, style: <#T##UIBarButtonItem.Style#>, target: <#T##Any?#>, action: <#T##Selector?#>)
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+
+        toolBar.setItems([spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+//        textField.inputView = picker
+        textField.inputAccessoryView = toolBar
+        
+        return textField
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<TextFieldWithPickerAsInputView>) {
+        uiView.text = text
+    }
+    
+    class Coordinator: NSObject, UIPickerViewDataSource, UIPickerViewDelegate , UITextFieldDelegate {
+        
+        private let parent : TextFieldWithPickerAsInputView
+        
+        init(textfield : TextFieldWithPickerAsInputView) {
+            self.parent = textfield
+        }
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return self.parent.data.count
+        }
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return self.parent.data[row]
+        }
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            self.parent.$selectionIndex.wrappedValue = row
+            self.parent.text = self.parent.data[self.parent.selectionIndex]
+            //               self.parent.textField.endEditing(true)
+        }
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            self.parent.textField.resignFirstResponder()
+        }
+    }
 }
--- SeoulLost/SeoulLost/Tabs/LostItemView/LostItemTabViewModel.swift
+++ SeoulLost/SeoulLost/Tabs/LostItemView/LostItemTabViewModel.swift
@@ -11,6 +11,7 @@ import SwiftUI
 import Combine
 
 class LostItemTabViewModel: ObservableObject {
+    //MARK: Enum
     enum State {
         case idle
         case loading
@@ -25,23 +26,20 @@ class LostItemTabViewModel: ObservableObject {
         case onFailedToLoadMovies(Error)
     }
     
+    //MARK: published propertys
     @Published var state = State.loading
-    @Published var isShowLoading = true
+    @Published var isShowLoading = false
     @Published var data:Array = Array<LostArticleModel>()
+    @Published var place:LostPlaceType = .bus
+    @Published var lostArticleType:LostArticleType = .wallet
     
+    @Published var placeTxt:String = ""
+    @Published var lostArticleTypeTxt:String = ""
+    
+    //MARK: propertys
     private var bag = Set<AnyCancellable>()
-    
     private let input = PassthroughSubject<Event, Never>()
-    
-    init() {
-        print("init!")
-        
-        request()
-    }
-    
-    func send(event: Event) {
-        input.send(event)
-    }
+    private var currentListIndex:Int = 0
     
     private var loadingStateCheck: AnyPublisher<Bool, Never> {
         $state
@@ -51,13 +49,90 @@ class LostItemTabViewModel: ObservableObject {
         .eraseToAnyPublisher()
     }
     
+    //MARK: lifecycle
+    init() {
+        request()
+        initTextProperty()
+    }
+    
+    //MARK: func
+    func send(event: Event) {
+        input.send(event)
+    }
+    
+    private func initTextProperty() {
+        switch self.place {
+        case .bus:
+            self.placeTxt = "버스"
+            break
+        case .corporateTaxi:
+            self.placeTxt = "법인택시"
+            break
+        case .korail:
+            self.placeTxt = "코레일"
+            break
+        case .privateTaxi:
+            self.placeTxt = "개인택시"
+            break
+        case .subway1_4:
+            self.placeTxt = "지하철(1~4호선)"
+            break
+        case .subway5_8:
+            self.placeTxt = "지하철(5~8호선)"
+            break
+        case .subway9:
+            self.placeTxt = "지하철(9호선)"
+            break
+        case .villageBus:
+            self.placeTxt = "마을버스"
+            break
+        }
+        
+        switch self.lostArticleType {
+        case .backpack:
+            self.lostArticleTypeTxt = LostArticleType.backpack.rawValue
+            break
+        case .bag:
+            self.lostArticleTypeTxt = LostArticleType.bag.rawValue
+            break
+        case .book:
+            self.lostArticleTypeTxt = LostArticleType.book.rawValue
+            break
+        case .briefcase:
+            self.lostArticleTypeTxt = LostArticleType.briefcase.rawValue
+            break
+        case .cellPhone:
+            self.lostArticleTypeTxt = LostArticleType.cellPhone.rawValue
+            break
+        case .clothing:
+            self.lostArticleTypeTxt = LostArticleType.clothing.rawValue
+            break
+        case .etc:
+            self.lostArticleTypeTxt = LostArticleType.etc.rawValue
+            break
+        case .file:
+            self.lostArticleTypeTxt = LostArticleType.file.rawValue
+            break
+        case .wallet:
+            self.lostArticleTypeTxt = LostArticleType.wallet.rawValue
+            break
+        case .shoppingBag:
+            self.lostArticleTypeTxt = LostArticleType.shoppingBag.rawValue
+            break
+        }
+    }
+    
+    
+    
+    //MARK: rest api func
     func request() {
-        let apiAddr = APIDefine.getLostArticleAPIAddress(startIndex: 1, endIndex: 5, type: .wallet, place: .bus, searchTxt: nil)
+        self.isShowLoading = true
+        let apiAddr = APIDefine.getLostArticleAPIAddress(startIndex: self.currentListIndex, endIndex: self.currentListIndex + 10, type: self.lostArticleType, place: self.place, searchTxt: nil)
         //        print("apiAddr:\(apiAddr)")
         DataApiManager.requestGETURL(apiAddr, headers: nil, success: { (result) in
 //            print("result:\(result)")
-            usleep(2 * 1000 * 1000)
-            print("받았다")
+            self.currentListIndex += 10
+            usleep(1 * 1000 * 1000) // 로딩을 보여주기위한 타이머 사실 쓸모없는 코드
             self.data.removeAll()
             for i in 0..<result["SearchLostArticleService"]["row"].count {
                 let item = result["SearchLostArticleService"]["row"][i]
